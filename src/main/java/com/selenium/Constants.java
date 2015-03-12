e31e7a6741d5c0097284870e55fe34edcc7b5196
Justin Phlegar <Justin.Phlegar@orasi.com>
2015-03-12 12:31:26 -0400

    Created project Constants class

---
+++ src/main/java/com/selenium/Constants.java
@@ -0,0 +1,26 @@
+package com.selenium;
+
+public class Constants {
+    /** Location of the environment URLs properties file */
+    final static public String ENVIRONMENT_URL_PATH = "EnvironmentURLs";
+   		
+    
+    /** Location of the user credentials properties file */
+    final static public String USER_CREDENTIALS_PATH = "UserCredentials";
+    
+    final static public String SANDBOX_PATH = "/sandbox/";
+    
+    /** Location of data provider files in the project  */
+    final static public String BLUESOURCE_DATAPROVIDER_PATH = "/bluesource/dataProviders/";
+    final static public String GOOGLEAPI_DATAPROVIDER_PATH = "/googleApi/dataProviders/";
+    final static public String PREDIC8API_DATAPROVIDER_PATH = "/predic8/dataProviders/";
+    
+    /** Location of drivers in project */
+    final static public String DRIVERS_PATH_LOCAL = "/drivers/";
+    final static public String DRIVERS_PATH_REMOTE = "C:\\Selenium\\WebDrivers\\";
+    
+    /** Location of tnsnames in project */
+    final static public String TNSNAMES_PATH = "/database/";
+  
+}
+
